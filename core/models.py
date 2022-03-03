d5cb6347e06f9497075ffcd13ef31b189327ebcd
ackleywill <ackleywill@yahoo.com.br>
2022-03-03 09:14:25 -0300

    Adicionadas funcões de alteração e exclusão

--- core/models.py
+++ core/models.py
@@ -9,6 +9,7 @@ class Evento(models.Model):
     descricao = models.TextField(blank=True,null=True)
     data_evento = models.DateTimeField(verbose_name="Data do Evento")
     data_criacao = models.DateTimeField(auto_now=True)
+    local = models.TextField(blank=True, null=True)
     usuario = models.ForeignKey(User, on_delete = models.CASCADE)
 
 
@@ -20,3 +21,6 @@ class Evento(models.Model):
 
     def get_data_evento(self):
         return self.data_evento.strftime('%d/%m/%Y %H:%M')
+
+    def get_data_input_evento(self):
+        return self.data_evento.strftime('%Y-%m-%dT%H:%M')
--- core/views.py
+++ core/views.py
@@ -50,17 +50,44 @@ def lista_eventos(request):
 
 @login_required(login_url='/login/') # Requer que o usuário esteja autenticado
 def evento(request):
-    return render(request, 'evento.html')
+    id_evento = request.GET.get('id')
+    dados = {} # dicionario vazio
+    if id_evento:
+        dados['evento'] = Evento.objects.get(id=id_evento)
+    return render(request, 'evento.html', dados)
 
 @login_required(login_url='/login/') # Requer que o usuário esteja autenticado
 def submit_evento(request):
     if request.POST:
         titulo = request.POST.get('titulo')
+        local = request.POST.get('local')
         data_evento = request.POST.get('data_evento')
-        descricao = request.POST.get( 'descricao')
+        descricao = request.POST.get('descricao')
         usuario = request.user
-        Evento.objects.create(titulo=titulo,
-                              data_evento=data_evento,
-                              descricao=descricao,
-                              usuario=usuario)
+        id_evento = request.POST.get('id_evento')
+        if id_evento:
+            #Evento.objects.filter(id=id_evento).update(titulo=titulo, data_evento=data_evento, descricao=descricao);
+            evento = Evento.objects.get(id=id_evento)
+            if evento.usuario == usuario:
+                evento.titulo = titulo
+                evento.local = local
+                evento.descricao = descricao
+                evento.data_evento = data_evento
+                evento.save()
+        else:
+            Evento.objects.create(titulo=titulo,
+                                  local=local,
+                                  data_evento=data_evento,
+                                  descricao=descricao,
+                                  usuario=usuario)
+    return redirect('/')
+
+
+@login_required(login_url='/login/') # Requer que o usuário esteja autenticado
+def delete_evento(request, id_evento):
+    usuario = request.user
+    #Evento.objects.filter(id=id_evento).delete()
+    evento = Evento.objects.get(id=id_evento)
+    if usuario == evento.usuario:
+        evento.delete()
     return redirect('/')
